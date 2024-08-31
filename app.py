955ceee449f9dc74b0402302e8f3a598c04325cd
GreenHugeBrain <142096498+GreenHugeBrain@users.noreply.github.com>
2024-08-31 18:47:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/listings', methods=['GET'])
 def get_listings():
