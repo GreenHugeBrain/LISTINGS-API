06ce69e8125963c067279834efaf953eb672af36
GreenHugeBrain <142096498+GreenHugeBrain@users.noreply.github.com>
2024-08-31 19:02:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def get_listings():
     url = 'https://csfloat.com/api/v1/listings'
     headers = {
-        'Authorization': 'NqsXK0DnQA9zGQu90_vXyVDXnyPJq3qB',
-        'Content-Type': 'application/json'
+        'Authorization': 'Bearer NqsXK0DnQA9zGQu90_vXyVDXnyPJq3qB',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
     cookies = {
         'session': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGVhbV9pZCI6Ijc2NTYxMTk5MDk1Mzg0NjI2Iiwibm9uY2UiOjAsImltcGVyc29uYXRlZCI6ZmFsc2UsImlzcyI6ImNzdGVjaCIsImV4cCI6MTcyNTU0Nzk0NX0.1SHRlZKbXXtIaHMz7QK8TNpOu2v8kbaFA9o2UvPhDIw'
